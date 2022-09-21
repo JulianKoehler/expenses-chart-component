251a8314afe424db169376e80798104e9ec3585d
Julian Köhler <112172251+JulianKoehler@users.noreply.github.com>
2022-09-21 15:54:46 +0200

    Update app.js
    unfortunately mobile devices seem to not support fetching local JSON data, which is why I had to save the JSON within the JS file

--- app.js
+++ app.js
@@ -1,6 +1,39 @@
 'use strict'
 
-import spendings from './data.json' assert {type: 'json'}; // getting the data from the JSON, does not work without a local server (e.g. Liveserver VScode DOES work)
+//import spendingsJson from './data.json' assert {type: 'json'}; // getting the data from the JSON, does not work without a local server (e.g. Liveserver VScode DOES work)
+
+//unfortunately mobile devices seem to not support fetching local JSON data, which is why I had to save the JSON within the JS file
+const spendings = [
+    {
+      "day": "mon",
+      "amount": 17.45
+    },
+    {
+      "day": "tue",
+      "amount": 34.91
+    },
+    {
+      "day": "wed",
+      "amount": 52.36
+    },
+    {
+      "day": "thu",
+      "amount": 31.07
+    },
+    {
+      "day": "fri",
+      "amount": 23.39
+    },
+    {
+      "day": "sat",
+      "amount": 43.28
+    },
+    {
+      "day": "sun",
+      "amount": 25.48
+    }
+  ]
+
 
 // getting the balance div and total div
 const balanceDiv = document.querySelector(".balance__left-side__amount")
